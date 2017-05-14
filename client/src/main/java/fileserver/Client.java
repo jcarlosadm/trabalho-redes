1c3941ce21360d8dd718d48c7e365c1714989e39
jcarlos <carlosmaster10@gmail.com>
2017-05-14 14:00:48 -0300

    Client changed

--- client/src/main/java/fileserver/Client.java
+++ client/src/main/java/fileserver/Client.java
@@ -2,27 +2,38 @@ package fileserver;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
-import java.net.UnknownHostException;
 
 public class Client {
 
+	// TODO manipulate exceptions
 	public static void main(String[] args) throws Exception {
 		Socket clientSocket = new Socket("localhost", 6789);
 		
-		BufferedReader inFromUser = new BufferedReader(new InputStreamReader(
-				System.in));
+		BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+		DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+		BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 		
-		DataOutputStream outToServer = new DataOutputStream(
-				clientSocket.getOutputStream());
+		boolean exit = false;
+		while (exit == false) {
+			
+			String messageToServer = inFromUser.readLine();
+			outToServer.writeBytes(messageToServer + "\n");
+			
+			String response = inFromServer.readLine();
+			System.out.println(response + System.lineSeparator());
+			
+			if (messageToServer.toLowerCase().equals("exit")) {
+				System.out.println("exiting...");
+				clientSocket.close();
+				exit = true;
+			}
+		}
 		
-		BufferedReader inFromServer = new BufferedReader(new InputStreamReader(
-				clientSocket.getInputStream()));
-		
-		clientSocket.close();
-	
+		inFromUser.close();
+		outToServer.close();
+		inFromServer.close();
 	}
-	
+
 }
