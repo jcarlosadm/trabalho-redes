33f0a01b034ee4a222ed3914a35b5a16812487b3
jcarlos <carlosmaster10@gmail.com>
2017-05-14 14:01:04 -0300

    ClientThread changed

--- server/src/main/java/fileserver/ClientThread.java
+++ server/src/main/java/fileserver/ClientThread.java
@@ -1,5 +1,9 @@
 package fileserver;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Socket;
 
 public class ClientThread implements Runnable {
@@ -12,7 +16,27 @@ public class ClientThread implements Runnable {
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
+		try {
+			BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+			DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+			
+			while (true) {
+				String clientSentence = inFromClient.readLine();
+				System.out.println(clientSentence + System.lineSeparator());
+				
+				outToClient.writeBytes("ok" + "\n");
+				if (clientSentence.toLowerCase().trim().equals("exit")) {
+					System.out.println("exiting...");
+					break;
+				}
+			}
+			
+			inFromClient.close();
+			outToClient.close();
+		} catch (IOException e) {
+			System.out.println("error on client thread. exiting.");
+			e.printStackTrace();
+		}
 
 	}
 
