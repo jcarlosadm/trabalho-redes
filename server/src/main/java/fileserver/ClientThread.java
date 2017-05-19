faf52202821e83ea2972577edce1c1a204a5a53e
jcarlos <carlosmaster10@gmail.com>
2017-05-19 01:58:12 -0300

    Uses writeUTF instead of writeBytes, and readUTF instead readLine, in ClientThread

--- server/src/main/java/fileserver/ClientThread.java
+++ server/src/main/java/fileserver/ClientThread.java
@@ -1,9 +1,8 @@
 package fileserver;
 
-import java.io.BufferedReader;
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.Socket;
 
 public class ClientThread implements Runnable {
@@ -16,22 +15,35 @@ public class ClientThread implements Runnable {
 
 	@Override
 	public void run() {
+		DataInputStream inFromClient = null;
+		DataOutputStream outToClient = null;
 		try {
-			BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
-			DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+			inFromClient = new DataInputStream(connectionSocket.getInputStream());
+			outToClient = new DataOutputStream(connectionSocket.getOutputStream());
 
 			while (true) {
-				String clientSentence = inFromClient.readLine();
-				System.out.println("-----------------------\n" + clientSentence + "-------------------------------");
-				
+				// TODO String clientSentence = inFromClient.readLine();
+				String clientSentence = inFromClient.readUTF();
+				System.out.println("-----------------------\n" + clientSentence + "\n-------------------------------");
+
 				String message = Protocol.execute(clientSentence);
 				if (message == null)
 					System.out.println("error to parse command.");
-				else
-					outToClient.writeBytes(message + "\n");
+				else {
+					// TODO outToClient.writeBytes(message + "\n");
+					outToClient.writeUTF(message);
+					outToClient.flush();
+				}
 			}
 		} catch (IOException e) {
 			System.out.println("client disconnected. closing thread.");
+		} finally {
+			try {
+				inFromClient.close();
+				outToClient.close();
+			} catch (Exception e1) {
+				System.out.println("error to close streams");
+			}
 		}
 
 	}
