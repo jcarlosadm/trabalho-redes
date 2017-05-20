9cbedab03f1a1c8d6357aa03a53e6b1e32084c38
jcarlos <carlosmaster10@gmail.com>
2017-05-20 19:36:41 -0300

    DeleteFolder implemented. DeleteFile changed

--- server/src/main/java/fileserver/command/DeleteFile.java
+++ server/src/main/java/fileserver/command/DeleteFile.java
@@ -3,6 +3,7 @@ package fileserver.command;
 import java.io.File;
 
 import fileserver.util.ClientState;
+import fileserver.util.FolderManager;
 
 public class DeleteFile extends Command {
 
@@ -14,18 +15,18 @@ public class DeleteFile extends Command {
 	public String run() throws Exception {
 		if (ClientState.getInstance().isLogged() == false)
 			return ResponseCode.NOT_LOGGED_IN.toString();
-		
+
 		String[] lines = clientData.split("\n");
-		String filepath = lines[1].substring(lines[1].indexOf(":") + 1).trim();
-		
+		String filepath = FolderManager.getInstance().getClientBasicPath() + "/"
+				+ lines[1].substring(lines[1].indexOf(":") + 1).trim();
+
 		File file = new File(filepath);
 		if (!file.exists() || file.isDirectory())
 			return ResponseCode.PATH_DONT_EXISTS.toString();
-		
+
 		if (file.delete() == false)
 			return null;
-			
-		
+
 		return ResponseCode.DELETE_SUCCESSFUL.toString();
 	}
 
--- server/src/main/java/fileserver/command/DeleteFolder.java
+++ server/src/main/java/fileserver/command/DeleteFolder.java
@@ -1,5 +1,10 @@
 package fileserver.command;
 
+import java.io.File;
+
+import fileserver.util.ClientState;
+import fileserver.util.FolderManager;
+
 public class DeleteFolder extends Command {
 
 	public DeleteFolder(String clientData) {
@@ -8,8 +13,21 @@ public class DeleteFolder extends Command {
 
 	@Override
 	public String run() throws Exception {
-		// TODO implement
-		return null;
+		if (ClientState.getInstance().isLogged() == false)
+			return ResponseCode.NOT_LOGGED_IN.toString();
+
+		String[] lines = clientData.split("\n");
+		String folderpath = FolderManager.getInstance().getClientBasicPath() + "/"
+				+ lines[1].substring(lines[1].indexOf(":") + 1).trim();
+
+		File folder = new File(folderpath);
+		if (!folder.exists() || !folder.isDirectory())
+			return ResponseCode.PATH_DONT_EXISTS.toString();
+
+		if (!folder.delete())
+			return null;
+
+		return ResponseCode.DELETE_SUCCESSFUL.toString();
 	}
 
 }
