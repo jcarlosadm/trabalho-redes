798e6b1de72b37fa62da04d808edcfdf25145a5b
jcarlos <carlosmaster10@gmail.com>
2017-05-20 19:24:28 -0300

    DeleteFile implemented

--- server/src/main/java/fileserver/command/DeleteFile.java
+++ server/src/main/java/fileserver/command/DeleteFile.java
@@ -1,16 +1,33 @@
 package fileserver.command;
 
+import java.io.File;
+
+import fileserver.util.ClientState;
+
 public class DeleteFile extends Command {
 
 	public DeleteFile(String clientData) {
 		super(clientData);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public String run() throws Exception {
 		// TODO Auto-generated method stub
-		return null;
+		if (ClientState.getInstance().isLogged() == false)
+			return ResponseCode.NOT_LOGGED_IN.toString();
+		
+		String[] lines = clientData.split("\n");
+		String filepath = lines[1].substring(lines[1].indexOf(":") + 1).trim();
+		
+		File file = new File(filepath);
+		if (!file.exists() || file.isDirectory())
+			return ResponseCode.PATH_DONT_EXISTS.toString();
+		
+		if (file.delete() == false)
+			return null;
+			
+		
+		return ResponseCode.DELETE_SUCCESSFUL.toString();
 	}
 
 }
